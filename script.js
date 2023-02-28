cb206d0fd4b4064cf7e8f099a6474d09db6b1166
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-02-28 11:45:00 +0100

    yeehaw

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ canvas.width = boardSize;
 canvas.height = boardSize;
 
 class Piece {
-  constructor({ position, color, imageSrc }) {
+  constructor({ position, color, type, worth, imageSrc }) {
     this.position = position;
     position = {
       rank: this.position.rank,
       file: this.position.file,
     };
-    this.color = color;
+    this.color = color; //1 = black | 0 = white
+    this.type = type; //King, Queen... etc
+    this.worth = worth; //Value of piece
     this.image = new Image();
     this.image.src = imageSrc;
   }
@@ -295,161 +297,225 @@ const arrayOfPieces = [
   (rook_black = new Piece({
     position: arrayOfSquares[56],
     color: "black",
+    type: "",
+    worth: 5,
     imageSrc: "./pieces/rook_black.png",
   })),
   (knight_black = new Piece({
     position: arrayOfSquares[57],
     color: "black",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/knight_black.png",
   })),
   (bishop_black = new Piece({
     position: arrayOfSquares[58],
     color: "black",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/bishop_black.png",
   })),
   (queen_black = new Piece({
     position: arrayOfSquares[59],
     color: "black",
+    type: "",
+    worth: 9,
     imageSrc: "./pieces/queen_black.png",
   })),
   (king_black = new Piece({
     position: arrayOfSquares[60],
     color: "black",
+    type: "",
+    worth: 0,
     imageSrc: "./pieces/king_black.png",
   })),
   (bishop_black2 = new Piece({
     position: arrayOfSquares[61],
     color: "black",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/bishop_black.png",
   })),
   (knight_black2 = new Piece({
     position: arrayOfSquares[62],
     color: "black",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/knight_black.png",
   })),
   (rook_black2 = new Piece({
     position: arrayOfSquares[63],
     color: "black",
+    type: "",
+    worth: 5,
     imageSrc: "./pieces/rook_black.png",
   })),
   (pawn_black = new Piece({
     position: arrayOfSquares[48],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black2 = new Piece({
     position: arrayOfSquares[49],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black3 = new Piece({
     position: arrayOfSquares[50],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black4 = new Piece({
     position: arrayOfSquares[51],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black5 = new Piece({
     position: arrayOfSquares[52],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black6 = new Piece({
     position: arrayOfSquares[53],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black7 = new Piece({
     position: arrayOfSquares[54],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (pawn_black8 = new Piece({
     position: arrayOfSquares[55],
     color: "black",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_black.png",
   })),
   (rook_white = new Piece({
     position: arrayOfSquares[0],
     color: "white",
+    type: "",
+    worth: 5,
     imageSrc: "./pieces/rook_white.png",
   })),
   (knight_white = new Piece({
     position: arrayOfSquares[1],
     color: "white",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/knight_white.png",
   })),
   (bishop_white = new Piece({
     position: arrayOfSquares[2],
     color: "white",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/bishop_white.png",
   })),
   (queen_white = new Piece({
     position: arrayOfSquares[3],
     color: "white",
+    type: "",
+    worth: 9,
     imageSrc: "./pieces/queen_white.png",
   })),
   (king_white = new Piece({
     position: arrayOfSquares[4],
     color: "white",
+    type: "",
+    worth: 0,
     imageSrc: "./pieces/king_white.png",
   })),
   (bishop_white2 = new Piece({
     position: arrayOfSquares[5],
     color: "white",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/bishop_white.png",
   })),
   (knight_white2 = new Piece({
     position: arrayOfSquares[6],
     color: "white",
+    type: "",
+    worth: 3,
     imageSrc: "./pieces/knight_white.png",
   })),
   (rook_white2 = new Piece({
     position: arrayOfSquares[7],
     color: "white",
+    type: "",
+    worth: 5,
     imageSrc: "./pieces/rook_white.png",
   })),
   (pawn_white = new Piece({
     position: arrayOfSquares[8],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white2 = new Piece({
     position: arrayOfSquares[9],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white3 = new Piece({
     position: arrayOfSquares[10],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white4 = new Piece({
     position: arrayOfSquares[11],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white5 = new Piece({
     position: arrayOfSquares[12],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white6 = new Piece({
     position: arrayOfSquares[13],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white7 = new Piece({
     position: arrayOfSquares[14],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
   (pawn_white8 = new Piece({
     position: arrayOfSquares[15],
     color: "white",
+    type: "",
+    worth: 1,
     imageSrc: "./pieces/pawn_white.png",
   })),
 ]; //32
@@ -506,7 +572,7 @@ readClick.addEventListener(
       startSquare = event.target.id;
     }
 
-    if (targetSquare && startSquare != undefined && targetSquare != startSquare) {
+    if (targetSquare && startSquare != undefined && targetSquare != startSquare && !hasClicked) {
       move(startSquare, targetSquare);
       startSquare = undefined;
       targetSquare = undefined;
